cdec8775fafd3982b4cd3111f5f9deb0a8144c60
Waterwoo <wuzichen@live.cn>
2018-01-26 16:22:22 +0800

    Add files via upload

--- sudoku.py
+++ sudoku.py
@@ -24,7 +24,7 @@ for i in range(81):
     arr[int(i/9)][i%9] = int(arry[i])
     
 def deal_num(x):
-    candidate = [[0 for m in range(9)] for n in range(9)]
+    candidate = [['0' for m in range(9)] for n in range(9)]
     for i in range(9):
         for j in range(9):
             if x[i][j] == 0:
@@ -41,6 +41,7 @@ def deal_num(x):
                             aa = str(x[k][l])
                             a = a.replace(aa, '')
                 candidate[i][j] = a
+                
                 
                                 
     return candidate
@@ -87,6 +88,9 @@ def row(x, y):
                         y[l][j] = k+1        
                         
 def nine_palace(x, y):
+    '''
+    nine_palace's function maybe repeated by col and row
+    '''
     for k in range(3):
         for l in range(3):
             stri_list = ''
@@ -102,25 +106,77 @@ def nine_palace(x, y):
                                 if str(x[row][col]).find(str(m+1)) != -1:
                                     y[row][col] = m+1
 
-'''
-nine_palace's function maybe repeated by col and row
-'''
-                                
+def x_wing_row(x):
+    
+    
+    stri_list = ['' for i in range(9)]
+    for i in range(9):
+        for j in range(9):
+            if x[i][j] != 0:
+                stri_list[i] += str(x[i][j])
+    for num in range(9):
+        a = [[0 for i in range(2)] for j in range(9)]
+        b = 0
+        for row in range(9):
+            if str(stri_list[row]).count(str(num+1)) == 2:
+                #print(row)
+                c = 0
+                for k in range(9):
+                    if str(x[row][k]).find(str(num+1)) != -1:
+                        a[row][c] = k
+                        c += 1
+                b += 1
+        if b > 1:
+                for n in range(9):
+                    for m in range(9):
+                        if a[n] == a[m] and n != m and a[n] != [0,0]:
+                            
+                            for l in range(9):
+                                if l != n and l != m:                                    
+                                    x[l][a[n][0]] = x[l][a[n][0]].replace(str(num+1), '')                                    
+                                    x[l][a[n][1]] = x[l][a[n][1]].replace(str(num+1), '')                                    
+    return x
     
 
+def x_wing_col(x):
+    stri_list = ['' for i in range(9)]
+    for j in range(9):
+        for i in range(9):
+            if x[i][j] != 0:
+                stri_list[j] += str(x[i][j])
+    for num in range(9):
+        a = [[0 for i in range(2)] for j in range(9)]
+        b = 0
+        for col in range(9):
+            if str(stri_list[col]).count(str(num+1)) == 2:
+                #print(row)
+                c = 0
+                for k in range(9):
+                    if str(x[k][col]).find(str(num+1)) != -1:
+                        a[col][c] = k
+                        c += 1
+                b += 1
+        if b > 1:
+                for n in range(9):
+                    for m in range(9):
+                        if a[n] == a[m] and n != m and a[n] != [0,0]:
+                            
+                            for l in range(9):
+                                if l != n and l != m:                                    
+                                    x[a[n][0]][l] = x[a[n][0]][l].replace(str(num+1), '')                                    
+                                    x[a[n][1]][l] = x[a[n][1]][l].replace(str(num+1), '')                                    
+    return x
+        
 co = 0
 while True:
-    num_select(deal_num(arr), arr)
-    column(deal_num(arr), arr)
-    row(deal_num(arr), arr)
-    nine_palace(deal_num(arr), arr)
+    numm = x_wing_row(x_wing_col(deal_num(arr)))
+    num_select(numm, arr)
+    column(numm, arr)
+    row(numm, arr)
+    nine_palace(numm, arr)
+    
     co += 1
-    if co > 20:
+    if co > 50:
+        
         break
 
-#delt_num_list = deal_num(arr)
-#num_select(delt_num_list, arr)
-#delt_num_list = deal_num(arr)
-#column(delt_num_list, arr)
-#delt_num_list = deal_num(arr)
-#row(delt_num_list, arr)
